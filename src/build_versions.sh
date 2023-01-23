a36cd0cd8919d002213183670d26bb13c46ff931
A.L.K <44205191+ALK222@users.noreply.github.com>
2023-01-23 13:37:41 +0100

    Automatización de compilado y traducción

---
+++ src/build_versions.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+LANGUAGES=("EN" "ES")
+OPTIONS=("BW" "COLOR")
+
+
+
+for language in "${LANGUAGES[@]}"; do
+    for option in "${OPTIONS[@]}"; do
+        xelatex \\def\\"$option"{} \\def\\"$language"{} \\input{cv} --jobname="$language-$option-cv.pdf"
+        mv cv.pdf ../cv/"$language-$option-cv.pdf"
+    done
+done
+
+
+rm *.log
+rm *.out
+rm *.aux
